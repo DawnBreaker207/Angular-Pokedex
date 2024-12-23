78a958f6b1beb42d7cd4ba2d69adfb6f70952270
DawnBreaker <tunganhngo207@gmail.com>
2024-12-23 21:14:07 +0700

    feat: fix code

--- src/app/common/services/pokemon-data.service.ts
+++ src/app/common/services/pokemon-data.service.ts
@@ -2,18 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable, shareReplay } from 'rxjs';
 import { Pokemon } from '../interface/pokemon';
+import { environment } from '@/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonDataService {
+  baseUrl = environment.apiUrl;
   pokemon: Observable<Pokemon[]>;
   constructor(private http: HttpClient) {
     this.pokemon = this.http
-      .get<Pokemon[]>('/api/pokemon')
-      // .get<Pokemon[]>('/api/pokemon.json')
-      .pipe(map(pokemon => pokemon.map(p => this.setPokemon(p))));
-    shareReplay(1);
+      .get<Pokemon[]>(`${this.baseUrl}/pokemon.json`)
+      .pipe(
+        map(pokemon => pokemon.map(p => this.setPokemon(p))),
+        shareReplay(1)
+      );
   }
   private setPokemon(pokemon: Pokemon) {
     pokemon.name = upperCaseName(pokemon.name);
