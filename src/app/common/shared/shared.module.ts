376f99bd930ca0a0254865ca5b252abed5e01917
DawnBreaker <tunganhngo207@gmail.com>
2024-12-24 01:55:34 +0700

    feat: update code

--- src/app/common/shared/shared.module.ts
+++ src/app/common/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { NgxModalModule } from '@ngx-lite/modal';
 import { FormSearchComponent } from './components/form-search/form-search.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
@@ -12,18 +11,22 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+
 const components = [FormSearchComponent, FooterComponent, HeaderComponent];
 const modules = [
   CommonModule,
   ReactiveFormsModule,
   RouterModule,
   NgxNavDrawerModule,
-  NgxModalModule,
   MatDialogModule,
   MatSidenavModule,
   MatIconModule,
   MatButtonModule,
   MatToolbarModule,
+  MatExpansionModule,
+  MatListModule,
 ];
 @NgModule({
   imports: [...modules],
